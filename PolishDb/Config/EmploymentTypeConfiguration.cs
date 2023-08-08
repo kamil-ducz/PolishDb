69c18f01650b161fb887d03aafe9b4dec792a9f1
Kamil D <kamilducz1989@gmail.com>
2023-08-08 10:26:54 +0200

    Define employment type dictionary

---
+++ PolishDb/Config/EmploymentTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PolishDb.Entities;
+
+namespace PolishDb.Config;
+public class EmploymentTypeConfiguration : IEntityTypeConfiguration<EmploymentType>
+{
+    public void Configure(EntityTypeBuilder<EmploymentType> builder)
+    {
+        builder.HasData(
+            new EmploymentType() { Id = (int)Enums.EmploymentType.MandateContract, Name = Enums.EmploymentType.MandateContract.ToString() },
+            new EmploymentType() { Id = (int)Enums.EmploymentType.ContractOfEmployment, Name = Enums.EmploymentType.ContractOfEmployment.ToString() }
+            );
+    }
+}
---
+++ PolishDb/Migrations/20230808082610_Define employment type dictionary table.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PolishDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class Defineemploymenttypedictionarytable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employments",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "MandateContract" },
+                    { 2, "ContractOfEmployment" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employments",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
